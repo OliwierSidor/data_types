371c819639b1a366963e3b5a56a4b17818de0cbb
Oliwier <oliwiersidor@gmail.com>
2022-05-08 16:06:36 +0200

    typyDanych

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+f
+6src/main/java/pl/arp4/typydanych/MainZadanie2Typy.java,8\0\80441a39ad6d2c54d518fac88d75ad83f81591fd
---
+++ src/main/java/pl/arp4/typydanych/MainZmienna.java
@@ -0,0 +1,11 @@
+package pl.arp4.typydanych;
+
+public class MainZmienna {
+    public static void main(String[] args) {
+        int liczba;
+
+        liczba = 5;
+
+        System.out.println(liczba);
+    }
+}
